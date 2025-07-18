e2dcaed3d37ac3b55edc9bb90647abecbf6ddeb0
Mohamed Dhia <Meddhia007@gmail.com>
2025-07-18 22:08:00 +0100

    Re-initialize and deploy backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,20 +1,9 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "dnaweb": {
+    "dnawebprod": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
-    }
-  },
-  "auth": {
-    "dnaweb01bcef93": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
     }
   }
 }
